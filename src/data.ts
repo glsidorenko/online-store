9ad5b023cb02b396b0cc25db183ee716cf71837e
Gleb <glsidorenko01@gmail.com>
2022-07-16 21:38:58 +0300

    feat: add filters

---
+++ src/data.ts
@@ -0,0 +1,113 @@
+export interface IDataItem {
+    [property: string]: string;
+    // name: string;
+    // quantity: string;
+    // year: string;
+    // brand: string;
+    // color: string;
+    // size: string;
+    // favorite: boolean;
+}
+
+export const data: IDataItem[] = [
+    {
+        num: '1',
+        name: 'Samsung Galaxy S21',
+        quantity: '2',
+        year: '2019',
+        brand: 'Samsung',
+        color: 'белый',
+        size: '3',
+        // favorite: false,
+    },
+    {
+        num: '2',
+        name: 'Samsung Galaxy A03',
+        quantity: '5',
+        year: '2000',
+        brand: 'Samsung',
+        color: 'красный',
+        size: '2',
+        // favorite: false,
+    },
+    {
+        num: '3',
+        name: 'Samsung Galaxy S20',
+        quantity: '3',
+        year: '2022',
+        brand: 'Samsung',
+        color: 'желтый',
+        size: '1',
+        // favorite: false,
+    },
+    {
+        num: '4',
+        name: 'Xiaomi Poco X4 pro',
+        quantity: '1',
+        year: '2014',
+        brand: 'Xiaomi',
+        color: 'желтый',
+        size: '2',
+        // favorite: false,
+    },
+    {
+        num: '5',
+        name: 'Xiaomi Redmi Note 11',
+        quantity: '4',
+        year: '2012',
+        brand: 'Xiaomi',
+        color: 'белый',
+        size: '1',
+        // favorite: true,
+    },
+    {
+        num: '6',
+        name: 'Xiaomi Redmi 9C 3/64gb',
+        quantity: '6',
+        year: '2010',
+        brand: 'Xiaomi',
+        color: 'красный',
+        size: '2',
+        // favorite: false,
+    },
+    {
+        num: '7',
+        name: 'Apple iPhone 11',
+        quantity: '12',
+        year: '2018',
+        brand: 'Apple',
+        color: 'белый',
+        size: '3',
+        // favorite: true,
+    },
+    {
+        num: '8',
+        name: 'Apple iPhone 13',
+        quantity: '10',
+        year: '2009',
+        brand: 'Apple',
+        color: 'красный',
+        size: '3',
+        // favorite: false,
+    },
+    {
+        num: '9',
+        name: 'Apple iPhone 12',
+        quantity: '2',
+        year: '2010',
+        brand: 'Apple',
+        color: 'желтый',
+        size: '3',
+        // favorite: false,
+    },
+    {
+        num: '10',
+        name: 'Apple iPhone SE',
+        quantity: '7',
+        year: '2004',
+        brand: 'Apple',
+        color: 'белый',
+        size: '1',
+        // favorite: false,
+    },
+];
---
+++ src/index.ts
@@ -0,0 +1,199 @@
+import { data, IDataItem } from './data';
+import './style.css';
+
+const checks = document.querySelectorAll('input[type="checkbox"]');
+
+class Application {
+    constructor() {
+        // checks.forEach((item) => {
+        //     item.addEventListener('change', (event) => {
+        //         // this.onClickEvent(item.na)
+        //         console.log(item);
+        //     });
+        // });
+        // sidebar.addEventListener('input', (event) => {
+        //     const target = event.target as HTMLInputElement;
+        //     target.addEventListener('click', (event) => {
+        //         this.onClickEvent(target.name, target.value);
+        //     });
+        //     console.log(target.name, target.value);
+        // });
+    }
+
+    onClickEvent(key: string, value: string): void {
+        const filteredData = products.getFilteredData(key, value);
+        this.renderItems(filteredData);
+    }
+
+    createHeader(): void {
+        const header = document.createElement('header');
+        header.classList.add('header');
+
+        header.innerHTML = `<div class="container">
+               <div class="logo">
+                 <img src="./img/shopping-cart.png" alt="Store Logo">
+               </div>
+               <h1 class="title">Online Store</h1>
+               <div class="backet">
+                 <img src="./img/shopping-cart.png" alt="Shopping Cart">
+               </div>
+             </div>`;
+
+        document.body.prepend(header);
+    }
+
+    createSidebarFilters(): void {
+        const sidebar = document.createElement('div');
+        const container = document.querySelector('main .container');
+        sidebar.classList.add('sidebar');
+
+        sidebar.innerHTML = `
+        
+            <div class="range">
+                <span>1</span>
+                <input type="range">
+                <span>2</span>
+            </div>
+        
+            <div>
+                <h4>Бренды</h4>
+                <div id="brands">
+                    <div class="checkbox"><label><input type="checkbox" name="brand" value="Apple"> Apple</label></div>
+                    <div class="checkbox"><label><input type="checkbox" name="brand" value="Samsung"> Samsung</label></div>
+                    <div class="checkbox"><label><input type="checkbox" name="brand" value="Xiaomi"> Xiaomi</label></div>
+                </div>
+            </div>
+            <div>
+                <h4>Цвета</h4>
+                <div id="colors">
+                    <div class="checkbox"><label><input type="checkbox" name="color" value="Черный"> Черный</label></div>
+                    <div class="checkbox"><label><input type="checkbox" name="color" value="Красный"> Красный</label></div>
+                    <div class="checkbox"><label><input type="checkbox" name="color" value="Белый"> Белый</label></div>
+                </div>
+            </div>
+            <div>
+                <h4>Количество камер</h4>
+                <div id="cameras">
+                    <div class="checkbox"><label><input type="checkbox" name="brands[]" value="3"> 3</label></div>
+                    <div class="checkbox"><label><input type="checkbox" name="brands[]" value="2"> 2</label></div>
+                    <div class="checkbox"><label><input type="checkbox" name="brands[]" value="1"> 1</label></div>
+                </div>
+            </div>
+        
+        `;
+
+        container?.prepend(sidebar);
+    }
+
+    createFooter() {
+        const footer = document.createElement('footer');
+
+        footer.innerHTML = `
+        <div class="container">
+          <p>© 2022</p>
+          <div class="logo">
+            <a href="">Rolling Scopes School</a>
+          </div>
+          <div class="github">
+            <a href="">Github</a>
+          </div>
+        </div>
+        `;
+        document.body.appendChild(footer);
+    }
+
+    renderItems(data: IDataItem[]): void {
+        const itemsContainer = document.querySelector('.items') as HTMLElement;
+        let htmlCatalog = '';
+
+        if (data.length === 0) {
+            itemsContainer.innerHTML = 'Nothing to show';
+            return;
+        }
+
+        data.forEach((elem: IDataItem) => {
+            // console.log(elem);
+            htmlCatalog += `
+            <div class="item">
+                <h2>${elem.name}</h2>
+                <div class="item-image">
+                    <img src=${'./'} alt="${elem.name}">
+                </div>
+                <div class="props">
+                    <h3>Количество: ${elem.quantity}</h3>
+                    <h3>Год: ${elem.year}</h3>
+                    <h3>Производитель: ${elem.shape}</h3>
+                    <h3>Цвет: ${elem.color}</h3>
+                </div>
+            </div>`;
+        });
+
+        itemsContainer.innerHTML = htmlCatalog;
+    }
+
+    render(data: IDataItem[]): void {
+        this.createFooter();
+        this.createHeader();
+        this.createSidebarFilters();
+        this.renderItems(data);
+    }
+}
+
+class Products {
+    data: IDataItem[];
+    filteredData: IDataItem[];
+
+    constructor(data: IDataItem[]) {
+        this.data = data;
+        this.filteredData = data;
+    }
+
+    getFilteredData(key: string, value: string): IDataItem[] {
+        const result: IDataItem[] = this.filteredData?.filter((elem) => {
+            if (elem[key]) {
+                return elem[key] === value;
+            } else {
+                console.log('no such key', key);
+            }
+        });
+
+        this.filteredData = result;
+
+        return result;
+    }
+}
+
+const sidebar = document.querySelector('#brands') as HTMLElement;
+
+const App = new Application();
+App.render(data);
+
+const products = new Products(data);
+
+// products.getFilteredData('brand', 'Samsung');
+
+// function filter() {
+//     // const checkChecked = document.querySelectorAll('input[type="checkbox"]:checked');
+//     const filter = search!.value.toLowerCase();
+
+//     const brands = [...sidebar.querySelectorAll('#brands input:checked')].map(
+//         (input) => (input as HTMLInputElement).value
+//     );
+//     const colors = [...sidebar.querySelectorAll('#colors input:checked')].map(
+//         (input) => (input as HTMLInputElement).value
+//     );
+//     const cameras = [...sidebar.querySelectorAll('#cameras input:checked')].map(
+//         (input) => (input as HTMLInputElement).value
+//     );
+
+//     console.log(brands, colors, cameras);
+
+//     // const newData = data.filter(
+//     //     (elem) =>
+//     //         (!brands.length || brands.includes(elem.shape)) &&
+//     //         (!colors.length || colors.includes(elem.color.toLocaleLowerCase())) &&
+//     //         (!cameras.length || cameras.includes(elem.size)) &&
+//     //         elem.name.toLocaleLowerCase().indexOf(filter) !== -1
+//     // );
+
+// }
